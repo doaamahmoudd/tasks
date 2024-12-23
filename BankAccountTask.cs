938e1ea8b54e0131d7fd5314b617b17d3e771c05
Doaa mahmoud <mahmouddoaa773@gmail.com>
2024-12-23 17:28:16 +0200

    Add files via upload

---
+++ BankAccountTask.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankaccount = new BankAccount(81365, 100000);
+            Console.WriteLine("Deposit...");
+            bankaccount.deposit(500);
+            Console.WriteLine("Withdraw...");
+            bankaccount.withdraw(1000);
+
+            Console.ReadKey();
+
+
+        }
+    }
+    class BankAccount
+    {
+        public int AccountNumer { get; set; }
+        private double Balance;
+        private readonly object _lock = new object();
+
+        public BankAccount(int accountnumber,double balance)
+        {
+            AccountNumer = accountnumber;
+            Balance = balance;
+        }
+        public void deposit(double amount)
+        {
+            lock (_lock)
+            {
+                Balance += amount;
+                Console.WriteLine("Amount of deposit: {0} , Balance become: {1}", amount, Balance);
+            }
+        }
+        public void withdraw(double amount)
+        {
+            if(Balance >= amount)
+            {
+                lock (_lock)
+                {
+                    Balance -= amount;
+                    Console.WriteLine("Amount of withdraw: {0} , Balance become: {1}", amount, Balance);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance...");
+            }
+
+        }
+        public double getBalance()
+        {
+            lock (_lock)
+            {
+                return Balance;
+            }
+           
+        }
+
+    }
+
+
+}
---
+++ ProblemSolviengTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemSolvingTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 10,20,23,45,96,10,21,22,21};
+
+            List<int> _list = new List<int>();
+            foreach(int num in list)
+            {
+                if(!_list.Contains(num))
+                {
+                    _list.Add(num);
+                }
+            }
+
+            List<int> reversedList = new List<int>();
+            for(int i=_list.Count-1;i>=0; i--)
+            {
+                reversedList.Add(_list[i]);
+                
+            }
+
+            Console.WriteLine(string.Join(" ,",list));
+            Console.WriteLine("After removing duplicates...");
+            Console.WriteLine(string.Join(" ,",_list));
+            Console.WriteLine("After reversing....");
+            Console.WriteLine(string.Join(" ,", reversedList));
+
+            Console.ReadKey();
+        }
+    }
+}
