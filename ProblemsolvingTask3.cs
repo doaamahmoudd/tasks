32e9348cf42f6bbc2a70a7c0efc81424f86d8c75
Doaa mahmoud <mahmouddoaa773@gmail.com>
2025-01-01 19:12:11 +0200

    Add files via upload

---
+++ ProblemsolvingTask3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the lenght of the text: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter the text to be encrypted: ");
+            string input = Console.ReadLine();
+            Console.Write("Enter the encryption key: ");
+            int key = int.Parse(Console.ReadLine());
+
+            key = key % 26;
+
+            string encrypted = "";
+
+            foreach (char c in input)
+            {
+                if(char.IsLetter(c))
+                {
+                    char baseChar =char.IsUpper(c)? 'A' : 'a';
+                    char newchar = (char) ((c- baseChar+ key) % 26+ baseChar);
+                    encrypted += newchar;
+                }
+                else
+                {
+                    encrypted += c;
+                }
+            }
+            Console.WriteLine(encrypted);
+            Console.ReadLine();
+        }
+    }
+}
